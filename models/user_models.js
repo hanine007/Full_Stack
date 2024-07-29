76f5b3fd53f67bc19000a3222e0c007bb32d77a8
hanine007 <bouchibahanine@gmail.com>
2024-07-29 22:09:33 +0100

    add user methods

--- models/user_models.js
+++ models/user_models.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+
  const User = new mongoose.Schema({
  name:{
     type:String,
@@ -31,9 +32,26 @@ import mongoose from "mongoose";
         this.hashed_password = this.encryptPassword(password);//e mot de passe est chiffré en utilisant la méthode encryptPassword(password)
     })
     .get(function() {
-        return this._password;
+        return this._password;//recup mdp en clair
     });
-
+User.methods={//methodes
+    authenticate: function(plainText) {
+    return this.encryptPassword(plainText)===this.hashed_password
+    },//Chiffre le mot de passe fourni (plainText) en utilisant la méthode encryptPassword et compare le résultat avec le mot de passe chiffré stocké (hashed_password). Retourne true si les deux correspondent, sinon false
+    encryptPassword:function (password){
+        if (!password) return '';
+        try{
+            return crypto.createHmac('sha1', this.salt).update(password).digest('hex') //Crée un objet HMAC avec l'algorithme sha1 et le sel pour update le mdp
+        }
+        catch(err){
+            return '';
+        }
+    },
+    makeSalt: function() {
+        return Math.round((new Date().valueOf() * Math.random())) + ''
+        }
+        
+}
  
 
 export const user = mongoose.model('user',User)
