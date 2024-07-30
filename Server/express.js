9e77e43c1fda330a3d322e7e49e70a99e3d403a1
hanine007 <bouchibahanine@gmail.com>
2024-07-30 15:07:13 +0100

    add routes

--- Server/express.js
+++ Server/express.js
@@ -1,18 +1,21 @@
+
 import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compression from 'compression'
+import route from '../routes/route.js'
  const app = express()
  app.use(bodyParser.json())
-app.use (bodyParser.urlencoded({extended:true}))
-app.use(cookieParser())
-app.use(compression())
-app.use(helmet())
-app.use(cors())
-
-app.get('/', (req, res) => {
+ app.use (bodyParser.urlencoded({extended:true}))
+ app.use(cookieParser())
+ app.use(compression())
+ app.use(helmet())
+ app.use(cors())
+ app.get('/', (req, res) => {
     res.status(200).send('Hello men!')
     })
+app.use ('/',route)
+
 export default app 
