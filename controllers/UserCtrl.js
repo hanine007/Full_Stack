193ef5dcbe34500492ad182ec139a8c796f28748
hanine007 <bouchibahanine@gmail.com>
2024-07-30 15:20:14 +0100

    a create fct

--- controllers/UserCtrl.js
+++ controllers/UserCtrl.js
@@ -1,4 +1,11 @@
 import {Useer }from '../models/user_models.js'
 export const CreateNewUser= (async(req,res)=>{
-
+    try{
+    const user = req.body
+    await Useer.create(user)
+return(res.status(200).send ('The user is created'))
+    }
+catch(error){
+ res.status(400).send({message:error.message})
+}
 })
